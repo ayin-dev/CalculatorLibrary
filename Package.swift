7d80052779c203504319953399fb4fbbb357ffe3
Alan Yin <ayin@darwinium.com>
2025-05-16 17:12:44 +1000

    first

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CalculatorLibrary",
+    platforms: [.iOS(.v18)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CalculatorLibrary",
+            targets: ["CalculatorLibrary"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "CalculatorLibrary",
+                      path: "Sources/Calculator_Framework.xcframework")
+
+    ]
+)
