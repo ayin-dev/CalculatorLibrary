77aa624cfb8cf9e98630820311750d2dd320f116
Alan Yin <ayin@darwinium.com>
2026-05-01 12:14:13 +1000

    add dependend package

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "CalculatorLibrary",
             targets: ["CalculatorLibrary"]),
     ],
+    dependencies: [
+        .package(id: "darwinium.package_test", from:"0.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
